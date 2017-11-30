fd9fbe80329891f7fc1ca7da60a7746dd1e9a7b7
glebmillenium <GodBlessYa95@gmail.com>
2017-11-30 09:30:36 +0800

    1) Добавлен класс Logger; 2) Исправлена дата вывода в файл лог

---
+++ client1/contact_center_application/core/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contact_center_application.core
+{
+	class Logger
+	{
+		private static string directoryLog = "log/log.txt";
+		private static string direcotryException = "log/exception.txt";
+		private static string directoryForTemporary = "tmp";
+
+		public static void initialize()
+		{
+			File.WriteAllText(directoryLog, "");
+			File.WriteAllText(direcotryException, "");
+			if (Directory.Exists(directoryForTemporary))
+			{
+				Directory.Delete(directoryForTemporary, true);
+				Directory.CreateDirectory(directoryForTemporary);
+			}
+		}
+
+		/// <summary>
+		/// Метод для журналирования действий пользователя
+		/// </summary>
+		/// <param name="message">Сообщение которое будет отправлено пользователю</param>
+		public static void log(string message)
+		{
+			StreamWriter writer = File.AppendText(directoryLog);
+			writer.Write(message + "\r\n");
+			writer.Close();
+		}
+	}
+}
